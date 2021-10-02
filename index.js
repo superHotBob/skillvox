2bc691118db56d0dea31b559a7c17ccd1ec14331
Дмитрий <19ham09@gmail.com>
2021-10-02 10:50:28 +0300

    add path

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 3000;
-const publicPath = path.join(__dirname, '..', 'server-skill/public');
+const publicPath = path.join(__dirname, '..', 'public');
 
-app.use(express.static(publicPath));
+app.use(cors());
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'));
