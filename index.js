773b778278e98623846746d051ff7e3162ad747d
Дмитрий <19ham09@gmail.com>
2021-10-27 08:19:34 +0300

    add_json

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3003;
 const publicPath = path.join(__dirname,'public');
 
 app.use(cors());
 
 app.use(express.static(__dirname + '/public'));
+
+app.get('/summary',(req,res) => {
+    res.sendFile(path.join(__dirname, 'summary.json'));
+});
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'));
