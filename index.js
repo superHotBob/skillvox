3b60f0d47fa381393ce400bb83c7b1bdfc6c6fe1
Дмитрий <19ham09@gmail.com>
2021-11-09 19:15:49 +0300

    next_version

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
-
+const fs = require('fs');
+const events = require('events');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 3003;
@@ -8,11 +9,25 @@ const publicPath = path.join(__dirname,'public');
 
 app.use(cors());
 
+const em = new events.EventEmitter();
+em.on('FirstEvent', function (data) {
+    console.log('First subscriber: ' + data);
+});
 
- 
+// Raising FirstEvent
+em.emit('FirstEvent', 'This is my first Node.js event emitter example.');
 
-// const httpServer = http.createServer(app);
+var myLogger = function (req, res, next) {
+    const my_data = new Date();
+    fs.appendFile('log.txt', my_data.toString() + '\n'  ,(err)=>{
+        if(err) console.log(err);
+    })
+    next();
+  };
+  
+app.use('/read_log',myLogger);
 app.use(express.static(__dirname + '/public'));
+
 
 app.get('/summary',(req,res) => {
     res.sendFile(path.join(__dirname, 'summary.json'));
@@ -20,11 +35,21 @@ app.get('/summary',(req,res) => {
 app.get('/summary_brand',(req,res) => {
     res.sendFile(path.join(__dirname, 'summary_brand.json'));
 });
+app.get('/read_log',(req,res) => {
+    fs.readFile('log.txt', 'utf8',function(err,data){
+        if( err )  err;
+        console.log(data);
+        res.sendFile(path.join(__dirname,'log.txt'));
+    });
+});
+    
 
 app.get('*', (req, res) => {
+    console.log(req.ip);
     res.sendFile(path.join(publicPath, 'index.html'));
 });
 
-app.listen(port, () => {
+app.listen(port, () => {   
+   
     console.log(`Server is up on port ${port}!`);
 });
