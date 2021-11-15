bea589c7eb6f005aae5cbcf1628f097f735bd75e
Дмитрий <19ham09@gmail.com>
2021-11-15 18:17:14 +0300

    18-17

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const fs = require('fs');
 const events = require('events');
+const rateLimit = require("express-rate-limit");
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 3003;
@@ -9,6 +10,12 @@ const publicPath = path.join(__dirname,'public');
 
 app.use(cors());
 
+const limiter = rateLimit({
+    windowMs: 20 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+    message: "Too many requests from this IP, please try again after a minute"
+});
+app.use(limiter);
 const em = new events.EventEmitter();
 em.on('FirstEvent', function (data) {
     console.log('First subscriber: ' + data);
@@ -19,15 +26,16 @@ em.emit('FirstEvent', 'This is my first Node.js event emitter example.');
 
 var myLogger = function (req, res, next) {
     const my_data = new Date();
-    fs.appendFile('log.txt', req.ip + ',' + my_data.toString() + '\n'  ,(err)=>{
+    
+    fs.appendFile('log.txt', req.originalUrl + ',' + my_data.toString() + '\n'  ,(err)=>{
         if(err) console.log(err);
     })
     next();
   };
   
-app.use('/read_log',myLogger);
-app.use(express.static(__dirname + '/public'));
 
+app.use(express.static(__dirname + '/public'));
+// app.use(myLogger);
 
 app.get('/summary',(req,res) => {
     res.sendFile(path.join(__dirname, 'summary.json'));
@@ -37,19 +45,20 @@ app.get('/summary_brand',(req,res) => {
 });
 app.get('/read_log',(req,res) => {
     fs.readFile('log.txt', 'utf8',function(err,data){
-        if( err )  err;
-        console.log(data);
+        if( err )  err;        
         res.sendFile(path.join(__dirname,'log.txt'));
     });
 });
     
 
-app.get('*', (req, res) => {
-    console.log(req.ip);
-    res.sendFile(path.join(publicPath, 'index.html'));
-});
-
+// app.get('*', (req, res) => {
+//     console.log(req.ip);
+//     res.write('<h1>Something broke!</h1>');
+// });
+// app.use(function(err, req, res, next) {
+//     console.error(err.stack);
+//     res.status(404).send('<h1>Something broke!</h1>');
+// });
 app.listen(port, () => {   
-   
     console.log(`Server is up on port ${port}!`);
 });
